a0c91b6d904c2ee6e417e22341e90ad068d5910a
João Pedro Alvarenga <joaopedroaats@pm.me>
2020-06-20 02:10:02 -0300

    create install.sh file

---
+++ install.sh
@@ -0,0 +1,29 @@
+ln -sv ~/dotfiles/.gitconfig ~/ 
+ln -sv ~/dotfiles/.profile ~/ 
+ln -sv ~/dotfiles/.zshrc ~/ 
+ln -sv ~/dotfiles/hosts /etc 
+mkdir ~/.config/redshift & ln -sv ~/dotfiles/redshift.conf ~/.config/redshift
+
+# Apps
+yay -S discord spotify zoom plank krita qbittorrent vlc
+
+# Browsers
+yay -S firefox-nightly firefox-developer-edition google-chrome tor-browser
+
+# Editors
+yay -S visual-studio-code-bin visual-studio-code-insiders android-studio netbeans intellij-idea-ultimate-edition 
+
+# Dev Programs
+yay -S reactotron devdocs-desktop insomnia postman 
+
+# Dev misc
+yay -S opam-git nvm-git deno-git yarn 
+
+# LAMP Config
+yay -S apache mysql php php-apache phpmyadmin
+
+# Npm: Dependencies
+yarn global add gatsby-cli bs-platform
+
+# Verify Updates
+yay -Syyu
